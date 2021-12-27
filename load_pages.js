f93d173e0fe149d14c07e733ac7a00a378d6b8d1
Yolo™ <66616587+IbimsnicesYolo@users.noreply.github.com>
2021-12-27 16:58:38 +0100

    add the installing instructions

--- load_pages.js
+++ load_pages.js
@@ -32,7 +32,7 @@ function loadcontent(content) {
   })();
 }
 
-contents["main"] = `
+contents["test"] = `
 <h1>YolosRP Wiki</h1>
 
 <h2>On the Left are the accessible Categorys</h2>
@@ -45,18 +45,38 @@ test code
 lol
 who cares
 </code></pre>
+<br>
+`;
+contents["main"] = `
+<h1>YolosRP Wiki</h1>
+
+<h2>On the Left are the accessible Categorys</h2>
 `;
 contents["install"] = `
-<h1>Installing the Gamemode</h1>
-
+<h1>Installing the Gamemode</h1><txt>
+<br>
+<br> 1. Add the Gamemode Content Pack (https://steamcommunity.com/sharedfiles/filedetails/?id=2563936353) to the Server Collection
+<br> 2. Download the Gamemode Files
+<br> 3. Extract them and Upload them to the garrysmod/gamemodes path
+<br> 4. Edit the Start Configuration of the Server to start with your desired Gamemode Example: starwarsrp
+<txt>
+<h1>Installing the DRM</h1>
+<txt>
+1.Paste this Lua Code in your Server Console and execute it:<br>
 <pre><code>
-Add the Gamemode Content Pack (https://steamcommunity.com/sharedfiles/filedetails/?id=2563936353) to the Server Collection
-Download the Gamemode Files
-Extract them and Upload them to the garrysmod/gamemodes path
-Edit the Start Configuration of the Server to start with your desired Gamemode Example: starwarsrp
-
-The Next step is installing the drm and setting the LicenseKey
+lua_run print((system.IsWindows()and"Windows"or system.IsLinux()and"Linux"or"Unsupported")..
+" ("..(jit.arch=="x64"and"x86-64"or"main").." branch)")
 </code></pre>
+<br>2. Install the corresponding DLL in garrysmod/lua/bin folder according to the Console Output
+<br>3. Start the Gamemode and Read the Console
+<br>4. After the Server Started completely there should be a message asking for the LicenseKey
+<br>5. Enter the LicenseKey in the Console and Restart the Server.
+<br>6. Done
+<txt>
 `;
+contents["debug"] = `
+<h1>YolosRP Wiki</h1>
 
+<h2>On the Left are the accessible Categorys</h2>
+`;
 loadcontent('main')
